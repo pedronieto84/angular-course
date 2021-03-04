ee1eecc5f46b2895d199f1681e949fed64603124
Pedro Nieto <pedro.nieto.sanchez@gmail.com>
2021-03-04 10:38:59 +0100

    instalados modulos de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { Componente1Component } from './components/componente1/componente1.component';
+
+
+const config = {
+  apiKey: "AIzaSyA5WsrCqkDVzq5RHUUl_G7fYQpe3EIsLcI",
+  authDomain: "angular-curso-4abd7.firebaseapp.com",
+  projectId: "angular-curso-4abd7",
+  storageBucket: "angular-curso-4abd7.appspot.com",
+  messagingSenderId: "905994794314",
+  appId: "1:905994794314:web:abb41a0ab2514d142ffdb3",
+  measurementId: "G-WFDXW3WLDS"
+}
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Componente1Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule 
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
